5354507c6be5817d5cac113507d8eeaf397bc44c
Nicolas Rozen <nicolas.rozen@rueducommerce.fr>
2016-10-06 15:30:09 +0200

    clean JmsFormErrorHandler

--- src/Rdc/Cart/CartApiBundle/Handler/FormErrorHandler.php
+++ src/Rdc/Cart/CartApiBundle/Handler/FormErrorHandler.php
@@ -15,69 +15,13 @@ use JMS\Serializer\Handler\FormErrorHandler as JMSFormErrorHandler;
 class FormErrorHandler extends JMSFormErrorHandler
 {
 
-    public function __construct(TranslatorInterface $translator)
-    {
-
-        $this->translator = $translator;
-    }
-
     public function serializeFormToJson(JsonSerializationVisitor $visitor, Form $form, array $type)
     {
         return $this->convertFormToArray($visitor, $form);
     }
 
-    public function serializeFormErrorToJson(JsonSerializationVisitor $visitor, FormError $formError, array $type)
-    {
-        return $this->getErrorMessage($formError);
-    }
-
-    private function getErrorMessage(FormError $error)
-    {
-        if (null !== $error->getMessagePluralization()) {
-            return $this->translator->transChoice(
-                $error->getMessageTemplate(),
-                $error->getMessagePluralization(),
-                $error->getMessageParameters(),
-                'validators'
-            );
-        }
-
-        return $this->translator->trans($error->getMessageTemplate(), $error->getMessageParameters(), 'validators');
-    }
-
     private function convertFormToArray(GenericSerializationVisitor $visitor, Form $data)
     {
-
-        $isRoot = null === $visitor->getRoot();
-
-        $form = new \ArrayObject();
-        $errors = array();
-        foreach ($data->getErrors() as $error) {
-            $errors[] = $this->getErrorMessage($error);
-        }
-
-        if ($errors) {
-            $form['errors'] = $errors;
-        }
-
-        $children = array();
-        foreach ($data->all() as $child) {
-            if ($child instanceof Form) {
-                $children[$child->getName()] = $this->convertFormToArray($visitor, $child);
-            }
-        }
-
-        if ($children) {
-            $form['children'] = $children;
-        }
-
-        if ($isRoot) {
-            $visitor->setRoot($form);
-        }
-
-        return $form;
-
-
         $form = $errors = array();
         $form['success'] = false;
         $form['code'] = 400;
