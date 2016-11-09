2289915381834a8f450399c9cd2a06159ba43a23
Nicolas Rozen <nicolas.rozen@rueducommerce.fr>
2016-11-09 16:15:28 +0100

    add comments on updateItems

--- src/Rdc/Cart/CartApiBundle/Controller/CartApiController.php
+++ src/Rdc/Cart/CartApiBundle/Controller/CartApiController.php
@@ -336,6 +336,23 @@ class CartApiController extends FOSRestController
      *      "offer_id": 3,
      *      "quantity": 3,
      *      "name": "clavier",
+     * 	    "categories": [{
+     *          "value": "8574448",
+     *          "key": "universe",
+     *          "additional_data": {
+     *              "name": "Ordinateurs",
+     *              "code": "MC-3540",
+     *              "value_code": "universe-hccid-30"
+     *          }
+     *      }, {
+     *          "value": "8574667",
+     *          "key": "area",
+     *          "additional_data": {
+     *              "name": "Ordinateur Portable",
+     *              "code": "MC-3759",
+     *              "value_code": "area-hccid-30-3"
+     *          }
+     *      }],
      *      "price": 100,
      *      "image": "http://www.rueducommerce.fr/mon_image.jpg",
      *      "merchant_id": 178,
